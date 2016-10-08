52e0ab609d425aff08d024fd41c3de80532f1d93
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-08 14:47:52 -0600

    create config dir for passport

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const passport = require(`passport`)
+const LocalAPIKeyStrategy = require(`passport-localapikey`).Strategy;
+const LinkedInStrategy = require(`passport-linkedin`).Strategy;
+
+passport.use(new LocalAPIKeyStrategy(
+  function(apikey, done) {
+    User.findOne({
+      apikey: apikey
+    }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.use(new LinkedInStrategy({
+    consumerKey: `78l9gagfa8y1e4`,
+    consumerSecret: `tebeZ6mNZqOrZOxd`,
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({
+      linkedinId: profile.id
+    }, function(err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,28 @@ const knex = require(`../knex`);
 const camelizeKey = require(`humps`);
 const decamelizeKeys = require(`humps`);
 const passport = require(`passport`)
-const localStrategy = require(`passport-local`).Strategy;
+const LocalAPIKeyStrategy = require(`passport-localapikey`).Strategy;
 
 /* GET users listing. */
-router.get(`/`, (req, res, next) => {
-  res.send(`respond with a resource`);
-});
+
+// router.get(`/`, (req, res, next) => {
+//
+// });
+
+// user signup with email
+router.post(`/`, (req, res, next) => {
+    const newUser = req.body;
+
+    // if anything is blank send error
+    if (newUser.email && newUser.password) {
+      // take info from newUser, put into passport localStrategy to create user
+      // then use knex(`users`).insert() to add stuff to db
+      // then save res.session
+      // then render user's contats page
+    } else {
+      // revisit...dont know how to handle error of missing email or pass
+      return (err => { next(err) });
+    }
+})
 
 module.exports = router;
