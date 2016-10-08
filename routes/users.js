bf06cb515f218eb35a9658cd1d1f33c9e7ee43f0
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-08 13:34:53 -0600

    lint errors

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,13 @@
 
 const express = require(`express`);
 const router = express.Router();
+const knex = require(`../knex`);
+const camelizeKey = require(`humps`);
+const decamelizeKeys = require(`humps`);
+const passport = require(`passport`)
+const localStrategy = require(`passport-local`).Strategy;
 
 /* GET users listing. */
-router.get(`/`, (req, res, next) => {
-  res.send(`respond with a resource`);
-});
-
 router.get(`/`, (req, res, next) => {
   res.send(`respond with a resource`);
 });
