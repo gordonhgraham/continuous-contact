29ef94466ddb1ac9099757dba9c73953c8fe7abb
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-09 13:41:59 -0600

    add get route for individual contact

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,19 @@ const express = require(`express`);
 const router = express.Router();
 
 /* GET home page. */
-router.get(`/`, (req, res, next) => {
-  res.render(`index`, { title: `Express` });
+router.get(`/:id`, (req, res, next) => {
+  const userId = req.session.id;
+  const contactId = req.params.id
+  const contact = {};
+
+  knex(`contacts`)
+    .where(`user_id`, userId)
+    .where(`id`, contactId)
+    .then(contact => {
+      contact.first_name = contact.first_name;
+
+      res.render(`individual.hbs`, {{all the stuff}})
+    })
 });
 
 module.exports = router;
