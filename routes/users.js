946619a8f6bf00810c7707f206b2ffe7639819f4
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-09 13:31:28 -0600

    add route in index.js for redirect to landing page, add psuedo code in users.js

--- routes/users.js
+++ routes/users.js
@@ -10,24 +10,26 @@ const LocalAPIKeyStrategy = require(`passport-localapikey`).Strategy;
 
 /* GET users listing. */
 
-// router.get(`/`, (req, res, next) => {
-//
-// });
+// read--after a login, display contacts page...res.redirect
+router.get(`/`, (req, res, next) => {
+  // if req.session, res.render contacts.hbs
+  // else, res.redirect to index.js
+});
 
 // user signup with email
 router.post(`/`, (req, res, next) => {
-    const newUser = req.body;
+  const newUser = req.body;
 
-    // if anything is blank send error
-    if (newUser.email && newUser.password) {
-      // take info from newUser, put into passport localStrategy to create user
-      // then use knex(`users`).insert() to add stuff to db
-      // then save res.session
-      // then render user's contats page
-    } else {
-      // revisit...dont know how to handle error of missing email or pass
-      return (err => { next(err) });
-    }
-})
+  // if anything is blank send error
+  if (newUser.email && newUser.password) {
+    // take info from newUser, put into passport localStrategy to create user
+    // then use knex(`users`).insert() to add stuff to db
+    // then save res.session
+    // then render user's contats page
+  } else {
+    // revisit...dont know how to handle error of missing email or pass
+    return err => { next(err); };
+  }
+});
 
 module.exports = router;
