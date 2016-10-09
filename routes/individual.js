c6a200db4c5106c1331ab604e80f8b1a09c535cc
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-09 14:02:55 -0600

    add routes for read, update, delete individual contact

--- routes/individual.js
+++ routes/individual.js
@@ -2,21 +2,62 @@
 
 const express = require(`express`);
 const router = express.Router();
+const knex = require(`../db/knex`);
 
-/* GET home page. */
+
+/* GET individual contact page. */
 router.get(`/:id`, (req, res, next) => {
-  const userId = req.session.id;
-  const contactId = req.params.id
-  const contact = {};
+  // const userId = req.session.id;
+  // uncomment and change .where(`user_id`, 1) to userId
+  const contactId = req.params.id;
 
   knex(`contacts`)
-    .where(`user_id`, userId)
+    .where(`user_id`, 1)
     .where(`id`, contactId)
     .then(contact => {
-      contact.first_name = contact.first_name;
+      res.send(contact);
 
-      res.render(`individual.hbs`, {{all the stuff}})
+      // res.render(`individual.hbs`, {{contact}})
     })
+    .next(err => {
+      if (err) { res.send(err); }
+    });
+});
+
+router.patch(`/:id`, (req, res, next) => {
+  // const userId = req.session.id;
+  // uncomment and change .where(`user_id`, 1) to userId
+  const contactId = req.params.id;
+  const updates = req.body;
+
+  knex(`contacts`)
+  .where(`user_id`, 1)
+  .where(`id`, contactId)
+  .update(updates, `*`)
+  .then(updatedContact => {
+    // res.render the updated Contact object
+    res.render(`individual.hbs`, updatedContact);
+  })
+  .catch(err => {
+    if (err) { res.send(err); }
+  });
+});
+
+router.delete(`/:id`, (req, res, next) => {
+  // const userId = req.session.id;
+  // uncomment and change .where(`user_id`, 1) to userId
+  const contactId = req.params.id;
+
+  knex(`contacts`)
+  .where(`user_id`, 1)
+  .where(`id`, contactId)
+  .del()
+  .then(() => {
+    res.redirect(`/contacts`);
+  })
+  .catch(err => {
+    if (err) { res.send(err); }
+  });
 });
 
 module.exports = router;
