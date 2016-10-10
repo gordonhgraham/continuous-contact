32ca9cac71daaf7e77b5643ff8f59db635afdf6b
mariajcb <bogomazm@gmail.com>
2016-10-10 12:14:13 -0600

    linked in oauth set up

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const path = require(`path`);
 const logger = require(`morgan`);
 const cookieParser = require(`cookie-parser`);
 const bodyParser = require(`body-parser`);
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 
 const routes = require(`./routes/index`);
 const users = require(`./routes/users`);
 const contacts = require(`./routes/contacts`);
 const individual = require(`./routes/individual`);
+const auth = require('./routes/auth');
+
 
 const app = express();
 
@@ -32,10 +37,46 @@ app.use(`/users`, users);
 app.use(`/contacts`, contacts);
 app.use(`/individual`, individual);
 
+//passport stuff
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env['SECRET_KEY']]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  //later this will be where you selectively send to the browser an identifier for your user, like their primary key from the database, or their ID from linkedin
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  //here is where you will go to the database and get the user each time from it's id, after you set up your db
+  done(null, obj);
+});
+
+passport.use(new LinkedInStrategy({
+    clientID: '78l9gagfa8y1e4',
+    clientSecret: 'tebeZ6mNZqOrZOxd',
+    callbackURL: "http://localhost:3000/auth/linkedin/callback",
+    scope: ['r_emailaddress', 'r_basicprofile'],
+    state: true,
+  },
+  function(token, tokenSecret, profile, done) {
+
+      // To keep the example simple, the user's LinkedIn profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the LinkedIn account with a user record in your database,
+      // and return that user instead (so perform a knex query here later.)
+      return done(null, profile);
+}));
+
+//mount auth.js middleware
+app.use('/auth', auth);
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error(`Not Found`);
-
   err.status = 404;
   next(err);
 });
---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+router.get('/linkedin',
+  passport.authenticate('linkedin', { state: 'SOME STATE'  }),
+  function(req, res){
+    // The request will be redirected to LinkedIn for authentication, so this
+    // function will not be called.
+});
+
+router.get('/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/'
+}));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
