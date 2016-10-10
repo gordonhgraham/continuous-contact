9b5f80cc9e76f0cc5bc7f942230b0cd2c9e0f5eb
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-10 11:22:55 -0600

    config passport-local, add to users.js get route to authenticate user

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const express = require(`express`);
+const session = require(`express-session`);
 const exphbs = require(`express-handlebars`);
 const path = require(`path`);
 const logger = require(`morgan`);
 const cookieParser = require(`cookie-parser`);
 const bodyParser = require(`body-parser`);
 const passport = require(`passport`);
-const LocalAPIKeyStrategy = require(`passport-localapikey`).Strategy;
+const LocalStrategy = require(`passport-local`).Strategy;
 const LinkedInStrategy = require(`passport-linkedin-oauth2`).Strategy;
 
 const routes = require(`./routes/index`);
@@ -20,39 +21,47 @@ const individual = require(`./routes/individual`);
 const app = express();
 
 // passport config--localStrategy
-passport.use(new LocalAPIKeyStrategy(
-  function(apikey, done) {
-    User.findOne({ apikey: apikey }, function (err, user) {
+passport.use(new LocalStrategy({
+  usernameField: `email`,
+  passwordField: `password`
+},
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
       if (err) { return done(err); }
-      if (!user) { return done(null, false); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
       return done(null, user);
     });
   }
 ));
 
-// passport config--LinkedinStrategy
-passport.use(new LinkedInStrategy({
-  clientID: `	78l9gagfa8y1e4`,
-  clientSecret: `tebeZ6mNZqOrZOxd`,
-  callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
-  scope: [`r_emailaddress`, `r_basicprofile`],
-}, function(accessToken, refreshToken, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
-}));
-
-app.get(`/auth/linkedin`,
-  passport.authenticate(`linkedin`, { state: `SOME STATE`  }),
-  function(req, res){
-    // The request will be redirected to LinkedIn for authentication, so this
-    // function will not be called.
-  });
+// // passport config--LinkedinStrategy
+// passport.use(new LinkedInStrategy({
+//   clientID: `	78l9gagfa8y1e4`,
+//   clientSecret: `tebeZ6mNZqOrZOxd`,
+//   callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+//   scope: [`r_emailaddress`, `r_basicprofile`],
+// }, function(accessToken, refreshToken, profile, done) {
+//   // asynchronous verification, for effect...
+//   process.nextTick(function () {
+//     // To keep the example simple, the user's LinkedIn profile is returned to
+//     // represent the logged-in user. In a typical application, you would want
+//     // to associate the LinkedIn account with a user record in your database,
+//     // and return that user instead.
+//     return done(null, profile);
+//   });
+// }));
+//
+// app.get(`/auth/linkedin`,
+//   passport.authenticate(`linkedin`, { state: `SOME STATE`  }),
+//   function(req, res){
+//     // The request will be redirected to LinkedIn for authentication, so this
+//     // function will not be called.
+//   });
 
 // view engine setup
 app.set(`views`, path.join(__dirname, `views`));
@@ -66,6 +75,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, `public`)));
+app.use(session({ secret: `crunchy taco` }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(`/`, routes);
 app.use(`/users`, users);
