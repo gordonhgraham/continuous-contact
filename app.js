8829f17a9c61624849025a584c9cf87869a22988
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-11 14:06:03 -0600

    lint app.js

--- app.js
+++ app.js
@@ -1,85 +1,83 @@
-require('dotenv').load()
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-var passport = require('passport');
-const session = require('express-session');
-var app = express();
+require(`dotenv`).load()
+const express = require(`express`);
+const path = require(`path`);
+const favicon = require(`serve-favicon`);
+const logger = require(`morgan`);
+const cookieParser = require(`cookie-parser`);
+const bodyParser = require(`body-parser`);
+const routes = require(`./routes/index`);
+const users = require(`./routes/users`);
+const LinkedInStrategy = require(`passport-linkedin-oauth2`).Strategy;
+const passport = require(`passport`);
+const session = require(`express-session`);
+const app = express();
 
-//passport strategy
+// passport strategy
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_CLIENT_ID,
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/linkedin/callback",
-  scope: ['r_emailaddress', 'r_basicprofile'],
+  scope: [`r_emailaddress`, `r_basicprofile`],
   state: true
-}, function(accessToken, refreshToken, profile, done) {
+}, (accessToken, refreshToken, profile, done) => {
   done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
 }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set(`views`, path.join(__dirname, `views`));
+app.set(`view engine`, `hbs`);
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(favicon(path.join(__dirname, `public`, `favicon.ico`)));
+
+app.use(logger(`dev`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, `public`)));
 app.use(session({
-  secret: 'keyboard cat',
+  secret: `keyboard cat`,
   resave: false,
-  saveUninitialized: false,
-}))
+  saveUninitialized: false
+}));
 
-//init passport
+// init passport
 app.use(passport.initialize());
+
 // passport session
 app.use(passport.session());
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete LinkedIn profile is
-//   serialized and deserialized.
-passport.serializeUser(function(user, done) {
+
+passport.serializeUser((user, done) => {
   done(null, user);
 });
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser((user, done) => {
   done(null, user)
 });
-app.get('/auth/linkedin',
-  passport.authenticate('linkedin'),
-  function(req, res){
+app.get(`/auth/linkedin`,
+  passport.authenticate(`linkedin`),
+  (req, res) => {
     // The request will be redirected to LinkedIn for authentication, so this
     // function will not be called.
-});
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-  successRedirect: '/',
-  failureRedirect: './views/error'
+  });
+app.get(`/auth/linkedin/callback`, passport.authenticate(`linkedin`, {
+  successRedirect: `/user`,
+  failureRedirect: `./views/error`
 }));
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   if (!req.session.passport) {
     app.locals.user = null;
-  }else {
+  } else {
     app.locals.user = req.session.passport.user;
   }
-  next()
-})
-app.use('/', routes);
-app.use('/users', users);
+  next();
+});
+app.use(`/`, routes);
+app.use(`/users`, users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error(`Not Found`);
+
   err.status = 404;
   next(err);
 });
@@ -87,20 +85,21 @@ app.use(function(req, res, next) {
 // error handlers
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get(`env`) === `development`) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render(`error`, {
       message: err.message,
       error: err
     });
   });
 }
+
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render(`error`, {
     message: err.message,
     error: {}
   });
