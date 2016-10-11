2179d426ad6be3852610a554f82d4932184d9574
Gordon H Graham <theboss@gordonhgraham.com>
2016-10-11 11:56:54 -0600

    add hard coded object to render in individual.js

--- routes/individual.js
+++ routes/individual.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get(`/`, (req, res, next) => {
-  res.render(`individual_contact`, { first_name: `Craig`, last_name: `Quincy` });
+  res.render(`individual_contact`, { first_name: `Craig`, last_name: `Quincy`, company: `Galvanize`, phone: `217-493-0549`});
 });
 
 module.exports = router;
