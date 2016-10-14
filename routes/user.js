98dfb01bad5e25e1db0c22986c3ec3b4616307f0
mariajcb <bogomazm@gmail.com>
2016-10-14 11:11:14 -0600

    fixing routes

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@
 const express = require(`express`);
 const router = express.Router();
 const knex = require(`../db/knex`);
+const bcrypt = require(`bcrypt`);
 
 /* GET home page. */
 router.get(`/`, (req, res, next) => {
@@ -14,38 +15,38 @@ router.get(`/`, (req, res, next) => {
     });
 });
 
-router.post(`/`, (req, res, next) => {
-  console.log('0-',req.body);
-  console.log(`1- route login`);
-    knex(`users`)
-        .where(`email`, req.body.email)
-        .first()
-        .then(function(results) {
-          if (results) {
-            console.log(`2-`, results);
-            let user = results;
-            let passwordMatch = bcrypt.compareSync(req.body.password, user.hashed_password);
-            // let passwordMatch = true;
-             console.log(`3-password match`,passwordMatch);
-             console.log('3.5-req.body.password',req.body.password,user.hashed_password);
-            if (req.body.password == user.hashed_password) {
-              console.log(`4-authenticated user`,user);
-              delete user.hashed_password;
-              // res.send(user);
-              console.log(`5-rendering`);
-              // res.render('user',{user:user})
-              res.render('user');
-            }
-            else {
-              console.log(`6-throwing new error`);
-              throw new Error(400, `Bad email or password`)
-            }
-          }
-        })
-        .catch((err) => {
-            next(err);
-        })
-});
+// router.post(`/`, (req, res, next) => {
+//   console.log('0-',req.body);
+//   console.log(`1- route login`);
+//     knex(`users`)
+//         .where(`email`, req.body.email)
+//         .first()
+//         .then(function(results) {
+//           if (results) {
+//             console.log(`2-`, results);
+//             let user = results;
+//             let passwordMatch = bcrypt.compareSync(req.body.password, user.hashed_password);
+//             // let passwordMatch = true;
+//              console.log(`3-password match`,passwordMatch);
+//              console.log('3.5-req.body.password',req.body.password,user.hashed_password);
+//             if (req.body.password == user.hashed_password) {
+//               console.log(`4-authenticated user`,user);
+//               delete user.hashed_password;
+//               res.send(user);
+//               console.log(`5-rendering`);
+//               // res.render('user',{user:user})
+//               // res.render('user');
+//             }
+//             else {
+//               console.log(`6-throwing new error`);
+//               throw new Error(400, `Bad email or password`)
+//             }
+//           }
+//         })
+//         .catch((err) => {
+//             next(err);
+//         })
+// });
 
 // router.post(`/addContact` (req, res, next) => {
 //   /* adds info from form to db */
